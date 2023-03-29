dd8ecfb340f8d59eadffc4fa78b872a28393c9c3
Adarsh Kumar Choudhary <128061818+Adarsh-120@users.noreply.github.com>
2023-03-29 06:10:00 +0000

    todos

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const http = require("http");
 
-const port = 8081;                                      //local port
+const port = 8084;                                      //local port
 // http://localhost:8081
+
+const toDoList = ["learn","apply things","succeed"];
+
+http
+  .createServer((req, res) =>{
+    const {method, url} = req;
+    console.log(method, url);
+    res.end();
+
+    if(url === "/todos"){
+        if(method === "GET"){
+            res.writeHead(200);
+            res.write(toDoList.toString());
+        }
+    }
+  })
 
 http
 .createServer((req, res) => {                         //call back function
