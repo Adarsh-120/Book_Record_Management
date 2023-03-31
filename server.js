25cea58068c8cef06538ecd48f6ee0c41a909661
Adarsh Kumar Choudhary <128061818+Adarsh-120@users.noreply.github.com>
2023-03-31 07:46:15 +0000

    Todos

--- server.js
+++ server.js
@@ -1,87 +1,42 @@
-const http = require("http");
+const express = require("express");
 
-const port = 8081;                                      //local port
+const app = express();
+app.use(express.json());
+
+const port = 8081;
+
+const toDoList = ["learn", "apply things", "succeed"];
+
 // http://localhost:8081
 
-const toDoList = ["learn","apply things","succeed"];
-
-http
-  .createServer((req, res) =>{
-    const {method, url} = req;
-    console.log(method, url);
-    
-
-    if(url === "/todos"){
-        if(method === "GET"){
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.write(toDoList.toString());
-        }
-        else if (method === "POST"){
-          let body = "";
-          req.on('error', (err) =>{
-              console.log(err);
-          })
-             .on('data', (chunk) =>{
-                 body += chunk;
-                // console.log(chunk);
-             })
-             .on('end',() =>{
-              body = JSON.parse(body);
-
-              let newToDo = toDoList;
-              newToDo.push(body.item);
-              //console.log(newToDo);
-              // console.log("data: ", body);
-             });
-        }
-        else if (method === "DELETE"){
-          let body = "";
-          req.on("error", (err) =>{
-              console.error(err);
-          })
-             .on("data", (chunk) =>{
-                 body += chunk;
-                // console.log(chunk);
-             })
-             .on("end", () =>{
-              body = JSON.parse(body);
-
-              let deleteThisItem = body.item;
-
-                 console.error(deleteThisItem);
-                  // for (let i= 0; i < toDoList.length; i++){
-                  //   if(toDoList[i] === deleteThisItem){
-                  //     toDoList.splice(i, 1);
-                  //     break;
-                  //   }
-                  // }                              
-                  toDoList.find((element, index) => {
-                    if (element === deleteThisItem){
-                            toDoList.splice(index, 1);
-                   }
-                   else{
-                      console.error("Error : Match Not Found !!");
-                   }
-                  });  
-             });          
-        }
-        else{
-          res.writeHead(501);
-        }
-    }
-    else{
-      res.writeHead(404);
-    }
-    res.end();
-  })
-
-       //http
-       // .createServer((req, res) => {                         //call back function
-       //     res.writeHead(200, { "Content-Type": text/html});
-       //     res.write("<h2>Hey Server Started :-) </h2>");
-       //     res.end();
-       // })
-.listen(port, () => {
-    console.log(`Nodejs Server Started running on Port ${port}`);
+app.get("/todos", (req, res) => {
+    res.status(202).send(toDoList);       //res.writeHead(200);   res.write(toDoList);
 });
-//Its not working
+
+
+app.post("/todos", (req, res) => {
+    let newToDoitem = req.body.name;
+    toDoList.push(newToDoitem);
+    res.status(202).send({message: "Task Added Successfully"});
+
+});
+
+app.delete("/todos", (req, res) => {
+    const deleteThisItem = req.body.name;
+
+    toDoList.find((elem, index) => {
+        if (elem === deleteThisItem){
+            toDoList.splice(index, 1);     
+        }
+        res.status(202).send({ message: `Deleted Item is: ${req.body.name}`});
+    });
+});
+
+app.all("*", (req, res) => {
+    res.status(202).send();
+});
+
+app.listen(port, () => {
+    console.log(`Nodejs Server Started running on port: ${port}` );
+});
+
